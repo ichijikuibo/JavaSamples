f885664442c38131193a859970d24926ced350f9
30004001 <30004001@B601-018.NWRC.NET>
2020-11-23 12:53:56 +0000

    initial

--- src/test/java/utilities/TextTest.java
+++ src/test/java/utilities/TextTest.java
@@ -12,22 +12,6 @@ import com.cc.utilities.Text;
 
 public class TextTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void testMonthStringInt() {
 		int[] input = {1,2,3,4,5,6,7,8,9,10,11,12,-1,0,13};
